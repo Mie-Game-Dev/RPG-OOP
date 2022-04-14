a94b609e7b4ffe82e645e152d7dc232f6be2ac88
Mie-Game-Dev <81910156+Mie-Game-Dev@users.noreply.github.com>
2022-04-14 10:34:08 +0800

    Initial commit

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,68 @@
+﻿using RPG.Core;
+using RPG.Movement;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour, IAction
+    {
+        [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttack = 1f;
+        [SerializeField] float weaponDamage = 10f;
+
+        Transform target;
+        float timeSinceLastAttack = 0;
+
+        private void Update()
+        {
+            timeSinceLastAttack += Time.deltaTime;
+
+            if (target == null) return;
+
+            if (!GetIsInRange())
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+            else
+            {
+                GetComponent<Mover>().Cancel();
+                AttackBehaviour();
+            }
+        }
+
+        private void AttackBehaviour()
+        {
+            if(timeSinceLastAttack > timeBetweenAttack)
+            {
+                // this will trigger Hit() event
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+            }
+
+        }
+
+
+        // Animation Event
+        void Hit()
+        {
+            if (target == null) return;
+            target.GetComponent<Health>().TakeDamage(weaponDamage);
+        }
+
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(transform.position, target.position) < weaponRange;
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            target = combatTarget.transform;
+        }
+
+        public void Cancel()
+        {
+            target = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.AI;
+using RPG.Core;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour, IAction
+    {
+        NavMeshAgent agent;
+        Animator animator;
+
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            animator = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            agent.SetDestination(destination);            
+            agent.isStopped = false;
+        }
+
+        public void Cancel()
+        {
+            agent.isStopped = true;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = agent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+
+            animator.SetFloat("forwardSpeed", speed);
+        }
+    }
+}
